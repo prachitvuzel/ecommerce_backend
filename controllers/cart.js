9a707ec74d580e8af4aa3f0529cbb5ae1ab3e581
prachitvuzel <prachit678bhujel@gmail.com>
2025-06-28 22:59:56 +0545

    product removal feature from cart added

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,16 +1,15 @@
 import CartModel from "../model/cart.js";
 import User from "../model/user.js";
-import Product from "../model/product.js"
+import Product from "../model/product.js";
 
 async function AddToCart(req, res) {
-    const productIdRaw = req.params.productId;
-    const productToAdd = await Product.findById(productIdRaw)
+  const productIdRaw = req.params.productId;
+  const productToAdd = await Product.findById(productIdRaw);
   const quantity = Number(req.params.quantity);
-    const user = await User.findById(req.user._id);
-    
+  const user = await User.findById(req.user._id);
 
-    if (!(user.userCart)) {
-    console.log(productToAdd)
+  if (!user.userCart) {
+    console.log(productToAdd);
     const cartCreated = await CartModel.Cart.create({
       cartHolder: req.user._id,
       productList: [
@@ -20,14 +19,14 @@ async function AddToCart(req, res) {
         },
       ],
     });
-      
-    await User.findByIdAndUpdate(req.user._id, {userCart: cartCreated._id}) 
+
+    await User.findByIdAndUpdate(req.user._id, { userCart: cartCreated._id });
     return res.json({ msg: "Product successfully added into cart" });
   } else {
     const userCart = await CartModel.Cart.findById(user.userCart);
     const cartProducts = userCart.productList;
     cartProducts.push({
-      ProductID: productToAdd._id ,
+      ProductID: productToAdd._id,
       quantity: quantity,
     });
 
@@ -39,4 +38,25 @@ async function AddToCart(req, res) {
   }
 }
 
-export default { AddToCart };
+async function DeleteItemsFromCart(req, res) {
+  try {
+    const product = await Product.findById(req.params.productId);
+    const productId = product._id;
+
+    console.log(productId);
+    const user = await User.findById(req.user._id);
+    const userCart = await CartModel.Cart.findById(user.userCart);
+
+    let productList = userCart.productList;
+    
+    let newProductList =  productList.filter((product) => (product.ProductID.toString() != productId.toString()))
+    
+    await CartModel.Cart.findByIdAndUpdate(userCart._id, {productList: newProductList });
+    return res.json({ msg: "product Deleted from Cart" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ msg: "Some error occured" });
+  }
+}
+
+export default { AddToCart, DeleteItemsFromCart };
--- routes/cart.js
+++ routes/cart.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.post("/addtocart/:productId/:quantity",middleware.baseAuthenticate("USER"), CartControllers.AddToCart)
 
 
-
+router.delete("/removeitems/:productId",middleware.baseAuthenticate("USER"),CartControllers.DeleteItemsFromCart)
 
 
 
